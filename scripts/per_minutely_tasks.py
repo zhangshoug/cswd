f42f29337ee7d57d276f8185e062d273a3e5c643
ldf <liu.dengfeng@hotmail.com>
2018-03-10 13:03:39 +0800

    修正判定

--- scripts/per_minutely_tasks.py
+++ scripts/per_minutely_tasks.py
@@ -1,29 +1,28 @@
-"""
-每分钟执行一次
-"""
-from cswd.websource.sina import fetch_globalnews
-from cswd.sqldata.db_schema import global_news
-from cswd.sqldata.base import get_engine
+    """
+    每分钟执行一次
+    """
+    from cswd.websource.sina import fetch_globalnews
+    from cswd.sqldata.db_schema import global_news
+    from cswd.sqldata.base import get_engine
 
-
-def fresh_globalnews():
-    """刷新全球财经信息，并存储在本地数据表"""
-    stamps, titles, categories, data_mid = fetch_globalnews()
-    engine = get_engine()
-    with engine.connect() as conn:
-        for i in range(len(stamps)):
-            if len(titles[i]):
+    def fresh_globalnews():
+        """刷新全球财经信息，并存储在本地数据表"""
+        stamps, titles, categories, data_mid = fetch_globalnews()
+        engine = get_engine()
+        with engine.connect() as conn:
+            for i in range(len(stamps)):
                 # 有时解析的标题内容为空
-                ins = global_news.insert().values(m_id = data_mid[i],
-                                                content=titles[i],
-                                                pub_time=stamps[i],
-                                                categories=categories[i])
-                try:
-                    conn.execute(ins)   
-                except:
-                    # 原始数据时间按降序排列，一旦重复即退出循环
-                    break
+                valid = (titles[i] is not None) and len(titles[i])
+                if valid:
+                    ins = global_news.insert().values(m_id=data_mid[i],
+                                                      content=titles[i],
+                                                      pub_time=stamps[i],
+                                                      categories=categories[i])
+                    try:
+                        conn.execute(ins)
+                    except:
+                        # 原始数据时间按降序排列，一旦重复即退出循环
+                        break
 
-
-if __name__=='__main__':
-    fresh_globalnews()
+    if __name__ == '__main__':
+        fresh_globalnews()
